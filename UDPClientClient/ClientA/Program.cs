8364eea43a00cf602345a0cf4a24e5c6ed079574
SirojKh <114700263+SirojKh@users.noreply.github.com>
2023-10-16 09:57:27 +0200

    U2

---
+++ UDPClientClient/ClientA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+public class ClientA
+{
+    public static void Main()
+    {
+        // Skapa en UDP-klient för att sända data
+        UdpClient udpClient = new UdpClient();
+
+        // Mål IP och port för att sända meddelandet
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+        // Skapa och serialisera objektet
+        Console.Write("Enter a message: ");
+        string content = Console.ReadLine();
+        Message message = new Message { Content = content };
+        string json = JsonConvert.SerializeObject(message);
+
+        // Skicka meddelandet
+        byte[] bytes = Encoding.UTF8.GetBytes(json);
+        udpClient.Send(bytes, bytes.Length, endPoint);
+
+        udpClient.Close();
+    }
+}
+
+public class Message
+{
+    public string Content { get; set; }
+}
---
+++ UDPClientClient/ClientB/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+public class ClientB
+{
+    public static void Main()
+    {
+        // Skapa en UDP-klient för att lyssna på en specifik port
+        UdpClient udpListner = new UdpClient(8080);
+
+        Console.WriteLine("Waiting for a message...");
+
+        // Ta emot meddelandet
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] bytes = udpListner.Receive(ref endPoint);
+        string json = Encoding.UTF8.GetString(bytes);
+
+        // Deserialisera objektet
+        Message message = JsonConvert.DeserializeObject<Message>(json);
+
+        Console.WriteLine($"Received: {message.Content}");
+
+        udpListner.Close();
+    }
+}
