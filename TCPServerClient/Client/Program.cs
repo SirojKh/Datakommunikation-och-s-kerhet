03ad94ae0cc47abc8ef1882953869f1e3aec526f
SirojKh <114700263+SirojKh@users.noreply.github.com>
2023-10-16 09:55:49 +0200

    U1

---
+++ TCPServerClient/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+
+public class Client
+{
+    public static void Main()
+    {
+        // Ansluter till servern på port 8080
+        TcpClient client = new TcpClient("127.0.0.1", 8080);
+        StreamWriter writer = new StreamWriter(client.GetStream());
+        StreamReader reader = new StreamReader(client.GetStream());
+
+        // Skapar en begäran och skickar den till servern
+        Console.Write("Enter a message: ");
+        string content = Console.ReadLine();
+        Request request = new Request
+        {
+            Content = content
+        };
+
+        string message = JsonConvert.SerializeObject(request);
+        writer.WriteLine(message);
+        writer.Flush();
+
+        // Läser svaret från servern
+        string responseMessage = reader.ReadLine();
+        Response response = JsonConvert.DeserializeObject<Response>(responseMessage);
+        Console.WriteLine($"Server responded: {response.Content}");
+
+        client.Close();
+    }
+}
---
+++ TCPServerClient/Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+
+public class Server
+{
+    public static void Main()
+    {
+        // Startar servern på port 8080
+        TcpListener server = new TcpListener(IPAddress.Any, 8080);
+        server.Start();
+        Console.WriteLine("Server started on port 8080...");
+
+        while (true)
+        {
+            // Väntar på inkommande klientanslutningar
+            TcpClient client = server.AcceptTcpClient();
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            // Läser inkommande meddelanden
+            string message = reader.ReadLine();
+            Request request = JsonConvert.DeserializeObject<Request>(message);
+
+            // Loggar och behandlar begäran
+            Console.WriteLine($"Received: {request.Content}");
+            Response response = new Response
+            {
+                Content = "Received: " + request.Content
+            };
+
+            // Svarar klienten
+            string responseMessage = JsonConvert.SerializeObject(response);
+            writer.WriteLine(responseMessage);
+            writer.Flush();
+            
+            client.Close();
+        }
+    }
+}
+
+public class Request
+{
+    public string Content { get; set; }
+}
+
+public class Response
+{
+    public string Content { get; set; }
+}
