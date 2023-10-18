f1051402e380b42fba8195ba79d1a94c7f2de05c
Siroj Khamidullaev <siroj@mbpsomthorsiroj.guest.stockholm.se>
2023-10-18 16:17:51 +0200

    U4

---
+++ Program 3.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using Microsoft.Extensions.Logging;
+
+public class Program
+{
+    private static SignalRService _signalRService;
+    private static SortedDictionary<string, string> _temperatures = new SortedDictionary<string, string>();
+
+    public static async Task Main(string[] args)
+    {
+        _signalRService = new SignalRService();
+        _signalRService.OnTemperatureReceived += UpdateTemperature;
+
+        await _signalRService.StartConnectionAsync();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+        await _signalRService.StopConnectionAsync();
+    }
+
+    private static void UpdateTemperature(string device, string temperature)
+    {
+        _temperatures[device] = temperature;
+        DisplayTemperatures();
+    }
+
+    private static void DisplayTemperatures()
+    {
+        Console.Clear();
+        Console.WriteLine("Temperature Monitor");
+        foreach (var entry in _temperatures)
+        {
+            Console.WriteLine($"{entry.Key}: Temperature: {entry.Value} °C");
+        }
+    }
+}
+
+// ... [SignalRService class here, unchanged] ...
+
+
+public class SignalRService
+{
+    private readonly HubConnection _hubConnection;
+    private readonly object _lock = new object();
+
+    // Händelse som utlöses när en temperatur mottas
+    public event Action<string, string> OnTemperatureReceived
+    {
+        add
+        {
+            lock (_lock)
+            {
+                _onTemperatureReceived += value;
+            }
+        }
+        remove
+        {
+            lock (_lock)
+            {
+                _onTemperatureReceived -= value;
+            }
+        }
+    }
+    private event Action<string, string> _onTemperatureReceived;
+
+    public SignalRService()
+    {
+        // Konfigurera SignalR-hubanslutning
+        _hubConnection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:5001/temperatureHub")
+            .WithAutomaticReconnect()
+            .ConfigureLogging(logging => 
+            {
+                logging.AddConsole();
+                logging.SetMinimumLevel(LogLevel.Information);
+            })
+            .Build();
+
+        // Hantera mottagna temperaturmeddelanden
+        _hubConnection.On<string, string>("ReceiveTemperature", (device, temperature) =>
+        {
+            _onTemperatureReceived?.Invoke(device, temperature);
+            Console.WriteLine($"Temperature Received: Device: {device}, Temperature: {temperature}°C");
+        });
+
+        // Hantera anslutningshändelser
+        _hubConnection.Closed += (error) =>
+        {
+            Console.WriteLine($"Connection Closed. Error: {error?.Message}");
+            return Task.CompletedTask;
+        };
+
+        _hubConnection.Reconnecting += (error) =>
+        {
+            Console.WriteLine($"Attempting to reconnect. Error: {error?.Message}");
+            return Task.CompletedTask;
+        };
+
+        _hubConnection.Reconnected += (connectionId) =>
+        {
+            Console.WriteLine($"Reconnected with Connection ID: {connectionId}");
+            return Task.CompletedTask;
+        };
+    }
+
+    // Starta SignalR-anslutning
+    public async Task StartConnectionAsync()
+    {
+        try
+        {
+            if (_hubConnection.State == HubConnectionState.Disconnected)
+            {
+                await _hubConnection.StartAsync();
+                Console.WriteLine("Connection Started");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Kunde inte starta anslutningen: " + e.Message);
+        }
+    }
+
+    // Stoppa SignalR-anslutning
+    public async Task StopConnectionAsync()
+    {
+        try
+        {
+            if (_hubConnection.State == HubConnectionState.Connected)
+            {
+                await _hubConnection.StopAsync();
+                Console.WriteLine("Connection Stopped");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Kunde inte stoppa anslutningen: " + e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Text.Json;
+
+class Program
+{
+    // Krypteringsnyckel och initialvektor för AES-kryptering
+    private static readonly byte[] Key = Convert.FromBase64String("EzErv+w4W9IOb9FCcK1wQ68Xml7PHD6KHIuJ+6bS/o4=");
+    private static readonly byte[] IV = Convert.FromBase64String("1IQ+GChzn/9Rr8MWfAH1Sw==");
+    
+    private static readonly List<string> Devices = new List<string> { "Device1", "Device2", "Device3", "Device4", "Device5" };
+
+    static async Task Main(string[] args)
+    {
+        // Skapa en anslutning till SignalR-hubben
+        var hubUrl = "https://localhost:5001/temperatureHub";
+        var hubConnection = new HubConnectionBuilder()
+            .WithUrl(hubUrl, options =>
+            {
+                options.HttpMessageHandlerFactory = (message) =>
+                {
+                    if (message is HttpClientHandler clientHandler)
+                        clientHandler.ServerCertificateCustomValidationCallback = 
+                            (sender, certificate, chain, sslPolicyErrors) => true;
+                    return message;
+                };
+            })
+            .Build();
+
+        await hubConnection.StartAsync();
+
+        string[] devices = new string[] { "Device1", "Device2", "Device3", "Device4", "Device5" };
+        
+        // Skicka temperaturdata för varje enhet med ett intervall på 1 sekund mellan varje enhet
+        while (true)
+        {
+            foreach (var device in devices)
+            {
+                var random = new Random();
+                var temperature = random.Next(-30, 50);
+
+                // Kryptera temperaturdata
+                var temperatureData = new TemperatureData
+                {
+                    DeviceName = device,
+                    EncryptedTemperature = EncryptString(temperature.ToString())
+                };
+
+                var serializedTemperatureData = JsonSerializer.Serialize(temperatureData);
+
+                await hubConnection.SendAsync("SendTemperature", serializedTemperatureData);
+                Console.WriteLine($"Skickade temperaturdata: {serializedTemperatureData}");
+                await Task.Delay(1000);
+            }
+
+            // Vänta 5 sekunder innan nästa uppdatering
+            await Task.Delay(5000);
+        }
+    }
+
+    // Funktion för att kryptera en sträng med AES-kryptering
+    static string EncryptString(string plainText)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Key;
+            aes.IV = IV;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(plainText);
+                    }
+
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+    }
+    
+    // Klass för att representera temperaturdata
+    public class TemperatureData
+    {
+        public string? DeviceName { get; set; }
+        public string? EncryptedTemperature { get; set; }
+    }
+}
